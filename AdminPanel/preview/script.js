91604c599f4f34dfea535c604f467ae7daea4db5
Ma1czi <04miki@wp.pl>
2023-05-10 00:23:16 +0200

    add printable page and some icons

--- AdminPanel/preview/script.js
+++ AdminPanel/preview/script.js
@@ -1,7 +1,15 @@
 function print(){
+    let background = document.getElementById('tabletoprint');
+    let borderRadius = background.style.borderRadius;
+    let border = background.style.border;
+    background.style.borderRadius = '0';
+    background.style.border = 'none';
     let doc = new jsPDF('p','pt','a4');
 
 doc.addHTML(document.getElementById('tabletoprint'),function() {
     doc.save('html.pdf');
 });
+}
+function back(){
+    window.location.href = '../';
 }
